ef02f9ddb119e98ab13caa7c3913feb527157a2a
WinteryFox <dragonhel5@gmail.com>
2019-05-23 16:34:02 +0200

    basic contest end manager

--- src/main/java/com/phaserush/gallerybot/ShardManager.kt
+++ src/main/java/com/phaserush/gallerybot/ShardManager.kt
@@ -1,8 +1,10 @@
 package com.phaserush.gallerybot
 
 import com.phaserush.gallerybot.data.database.Database
+import com.phaserush.gallerybot.data.database.Row
 import discord4j.core.DiscordClient
 import discord4j.core.`object`.entity.Channel
+import discord4j.core.`object`.entity.GuildMessageChannel
 import discord4j.core.`object`.presence.Activity
 import discord4j.core.`object`.presence.Presence
 import discord4j.core.`object`.util.Snowflake
@@ -13,10 +15,9 @@ import org.slf4j.LoggerFactory
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.time.Duration
-import java.time.Instant
 
 class ShardManager {
-    private val logger : Logger = LoggerFactory.getLogger(ShardManager::class.java)
+    private val logger: Logger = LoggerFactory.getLogger(ShardManager::class.java)
     private val database: Database = Database()
     private val eventHandler = EventHandler(database)
     private val shards: List<DiscordClient> = ShardingClientBuilder(config.token)
@@ -28,22 +29,30 @@ class ShardManager {
             .block()!!
 
     init {
-        /*Flux.interval(Duration.ofMinutes(1))
-                .map {
-                    database.get("SELECT * FROM contests")
-                            .map {
-                                if (Instant.ofEpochSecond(it.columns["contestEnd"] as Long).isAfter(Instant.now())) {
-                                    shards[0].getGuildById(Snowflake.of(it.columns["id"] as Long))
-                                            .map { guild ->
-                                                guild.systemChannel.map { channel ->
-                                                    channel.createMessage("Your contest is over punk!")
-                                                }
-                                            }
-                                }
+        createScheduledTask()
+    }
+
+    // TODO: Localize this
+    private fun createScheduledTask() {
+        Flux.interval(Duration.ofSeconds(5))
+                .flatMap { _ ->
+                    database.get("select * from contests where completed=? and unix_timestamp() > votingEndTime", false)
+                            .map(Row::columns)
+                            .flatMap { columns ->
+                                shards[0].getChannelById(Snowflake.of(columns["submissionChannelId"] as Long))
+                                        .ofType(GuildMessageChannel::class.java)
+                                        .flatMap { channel ->
+                                            if (columns["winnerId"] != null)
+                                                shards[0].getUserById(Snowflake.of(columns["winnerId"] as Long))
+                                                        .flatMap { channel.createMessage("Your contest is over punk!\nThe winner is ${it.mention}") }
+                                            else
+                                                channel.createMessage("The contest finished but nobody won :(")
+                                        }
+                                        .flatMap { database.set("update contests set completed=true where id=? and name=?", columns["id"] as Long, columns["name"]) }
                             }
                 }
                 .subscribe()
-        logger.info("Created scheduled time task")*/
+        logger.info("Created scheduled time task")
     }
 
     /**
